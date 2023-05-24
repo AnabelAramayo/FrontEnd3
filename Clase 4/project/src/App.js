2ba472079e3e2dd07753d252a3e84792f056830b
Anabel Aramayo <anabelaramayo@gmail.com>
2023-05-24 16:38:36 -0300

    Clase 4 asinc

---
+++ Clase 4/project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img alt ='nice man' src='https://github.com/Ivanszs/dh-frontend3-clases/blob/master/clase-4/satisfied.png?raw=true'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
